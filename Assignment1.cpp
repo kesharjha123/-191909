75ae4b5bdc8cec454bd106f052696970310fba66
kesharjha123 <kesharjha789@gmail.com>
2021-03-14 16:13:35 +0530

    my ass

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -3,27 +3,24 @@
 
 #include<iostream>
 using namespace std;
-char in;
+
+int n;
+
 int factorial(int x){
 	if(x>0){
 		return x*factorial(x-1);
-			}
-	 else if (x=0)
+		}
+		
+	else
 	return 1;
-	
-	else{
-		return in ;
-	}		
-	
+
 }
 
 int main(){
-     int n;
+	
      cout<<"enter a positive number"<<" ";
      cin>>n;
      cout <<"Factorial of"<<" "<<n<<" "<<"is"<<" "<<factorial(n)<<endl;
-     
-     return 0;
+
+return 0;
 }
-
-
